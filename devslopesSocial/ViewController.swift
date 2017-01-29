9a88691aa855ae85325db41a29f3ab38906c53d9
IamTinyTurtle <tss.abr@gmail.com>
2017-01-29 17:13:51 -0500

    Created Project
    created the project in Xcode

---
+++ devslopesSocial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devslopesSocial
+//
+//  Created by Adam Ribblett on 1/29/17.
+//  Copyright © 2017 IamAdamRibblett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
